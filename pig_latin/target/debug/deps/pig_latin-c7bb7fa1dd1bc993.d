b22a2e7f0f3d0b74af532e11ff6bcb727705e3ac
Kashina-Elina <115581776+Kashina-Elina@users.noreply.github.com>
2024-12-22 15:02:31 +0300

    Строка в кодировку "поросячьей латыни"

---
+++ pig_latin/target/debug/deps/pig_latin-c7bb7fa1dd1bc993.d
@@ -0,0 +1,5 @@
+C:\Users\KME\Desktop\20321\pig_latin\target\debug\deps\pig_latin-c7bb7fa1dd1bc993.exe: src/main.rs
+
+C:\Users\KME\Desktop\20321\pig_latin\target\debug\deps\pig_latin-c7bb7fa1dd1bc993.d: src/main.rs
+
+src/main.rs:
---
+++ pig_latin/target/debug/pig_latin.d
@@ -0,0 +1,1 @@
+C:\Users\KME\Desktop\20321\pig_latin\target\debug\pig_latin.exe: C:\Users\KME\Desktop\20321\pig_latin\src\main.rs
