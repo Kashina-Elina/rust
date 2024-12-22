5c7454bf537aa5ab98ec2086aeec8d95a95bc563
Kashina-Elina <115581776+Kashina-Elina@users.noreply.github.com>
2024-12-22 14:53:47 +0300

    Генерирование n-го числа Фибоначчи.

---
+++ n_fib/target/debug/deps/n_fib-99b77be9acc55ff8.d
@@ -0,0 +1,5 @@
+C:\Users\KME\Desktop\20321\n_fib\target\debug\deps\n_fib-99b77be9acc55ff8.exe: src/main.rs
+
+C:\Users\KME\Desktop\20321\n_fib\target\debug\deps\n_fib-99b77be9acc55ff8.d: src/main.rs
+
+src/main.rs:
---
+++ n_fib/target/debug/n_fib.d
@@ -0,0 +1,1 @@
+C:\Users\KME\Desktop\20321\n_fib\target\debug\n_fib.exe: C:\Users\KME\Desktop\20321\n_fib\src\main.rs
