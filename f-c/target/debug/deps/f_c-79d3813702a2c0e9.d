ea709821dad4e21e265c0237a8ef56e2ded97a67
Kashina-Elina <115581776+Kashina-Elina@users.noreply.github.com>
2024-12-22 14:52:05 +0300

    Конвертация температур по Фаренгейту к Цельсию.

---
+++ f-c/target/debug/deps/f_c-79d3813702a2c0e9.d
@@ -0,0 +1,5 @@
+C:\Users\KME\Desktop\20321\f-c\target\debug\deps\f_c-79d3813702a2c0e9.exe: src/main.rs
+
+C:\Users\KME\Desktop\20321\f-c\target\debug\deps\f_c-79d3813702a2c0e9.d: src/main.rs
+
+src/main.rs:
---
+++ f-c/target/debug/f-c.d
@@ -0,0 +1,1 @@
+C:\Users\KME\Desktop\20321\f-c\target\debug\f-c.exe: C:\Users\KME\Desktop\20321\f-c\src\main.rs
